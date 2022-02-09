8308aba9922365d668cf71a3ee259955cca5d4cd
catalinux <opreacata17@gmail.com>
2022-02-09 12:53:34 +0200

    simplify install for nvim and gitconfig

---
+++ git/install.sh
@@ -0,0 +1,3 @@
+rm -rf ~/.gitconfig 2> /dev/null
+
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
--- nvim/install.sh
+++ nvim/install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p ~/.config/nvim/additional
-mkdir -p ~/.config/nvim/lua/cata
-mkdir -p ~/.config/nvim/lua/cata/lsp
-mkdir -p ~/.config/nvim/lua/cata/lsp/settings
 
-# link wholesale
-for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
-    rm -rf ~/.config/nvim/$f 2> /dev/null
-    ln -s ~/dotfiles/nvim/$f ~/.config/nvim/$f
-done
+rm -rf ~/.config/nvim 2> /dev/null
+
+ln -s ~/dotfiles/nvim ~/.config/nvim
